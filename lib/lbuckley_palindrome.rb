36b87e767876c15059b42b2c4e02c66756c6bf2d
Leah Buckley <leah@Leahs-MacBook-Air.local>
2021-03-21 12:53:51 +0000

    Initialize repository

---
+++ lib/lbuckley_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "lbuckley_palindrome/version"
+
+module LbuckleyPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/lbuckley_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module LbuckleyPalindrome
+  VERSION = "0.1.0"
+end
